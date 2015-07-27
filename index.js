18e6a0d77791326f931acffb542afe3656631736
Kaylyn <code@kaylynbogle.com>
2015-07-26 17:02:28 -0700

    stage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var webshot = require('webshot')
 var fs = require('fs')
 var dot = require('dot')
+dot.templateSettings.strip = false
 
 var width = 2466
 var height = 3366
@@ -9,6 +10,7 @@ var card = fs.readFileSync('card.dotjs.html', { encoding: 'utf8' })
 var tpl = dot.template(card)
 
 var result = tpl({
+	pick: 1,
 	content: 'A CORBA service written in Ada which communicates using JSONx'
 })
 
